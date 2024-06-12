a29bd859a9606af0ba408fee787d6efea53db3f8
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-06-12 16:13:45 -0700

    Add make_word_array and check_guess functions
    The make_word_array function takes the selected
    mystery word and returns a hash of each letter and
    its position in the word. The check_guess function
    checks the letter guess from the user against the
    word_array and returns that specific entry.

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,12 @@
 class Game
+    attr_accessor :word_array, :guesses
+
     def initialize(dictionary_file)
         #@save_file = get_save
         #@player
         @word_length = (5..12).to_a
         @guesses = 8
+        @word_array = {}
         @word = get_word(dictionary_file)
         @game_over = false
     end
@@ -17,6 +20,30 @@ class Game
                 word = entry
             end
         end
+        make_word_array(word)
         return word
+    end
+
+    def make_word_array(word)
+        split_word = word.split("")
+        split_word.each_with_index do |letter, index|
+            unless word_array.has_key?(letter.to_sym)
+                self.word_array[letter.to_sym] = [index]
+            else
+                self.word_array[letter.to_sym].push(index)
+            end
+        end
+    end
+
+    def check_guess(guess)
+        if word_array.has_key?(guess.to_sym)
+            correct_guess = [guess]
+            word_array[guess.to_sym].each do |position|
+                correct_guess.push(position)
+            end
+            return correct_guess
+        else
+            return nil
+        end
     end
 end
