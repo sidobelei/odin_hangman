a8657cb8d301b092ddb94b98080f741afc8fb4cc
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-06-12 09:58:44 -0700

    Add main.rb and dictionary file

---
+++ main.rb
@@ -0,0 +1,32 @@
+ #new game start load dictionary list
+ #new game start has an option to load a saved game file
+ #which would continue where you left off
+ #select a word that is between 5-12 chars long
+ #maybe used a rand to pick a number between 0-length of dictionary
+ #and an until till we get the right size word
+
+ 
+ #Hang man print out
+ # _____
+ #|     |
+ #|     o
+ #|    /|\
+ #|    / \
+ #|
+ #------
+
+ # _ r o g r a _ _ i n g  Guesses Left: 8
+ 
+ #gets input from user only a letter at a time, make
+ #sure input is one letter long and case insensitive
+ #have an option to accept a save input 
+
+ #have a player class keeps track of your guesses,
+ #your inputs
+
+ #have something to keep track of the target word
+
+ #board class state of the board at save i.e word,
+ #hangman figure, guesses
+
+ 
