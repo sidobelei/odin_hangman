64c143292f898b50b41f3071a58949e4b6630e8b
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-06-14 10:22:58 -0700

    Add hangman display updating functionality
    The update_display function calls on two other
    methods the update_word_display and update_hangman
    to update the word display and the hangman diagram
    given the user's input and status of the game.

--- lib/game_view.rb
+++ lib/game_view.rb
@@ -1,15 +1,24 @@
 class GameView
+    attr_accessor :word_display, :hangman_array
     def initialize(word)
         @hangman_array = [
             ["    _____"],
             ["   |     |"],
-            ["   |     ", "o"],
-            ["   |    ", "/", "|", "\\"],
-            ["   |    ", "/", " ", "\\"],
+            ["   |     ", ""],
+            ["   |    ", " ", "", ""],
+            ["   |    ", "", " ", ""],
             ["   |"],
             [" -----"]
         ]
         @word_display = Array.new(word.length, "_")
+        @hangman_parts = {
+            :"head" => ["o", 2, 1],
+            :"torso" => ["|", 3, 2],
+            :"left_arm" => ["/", 3, 1],
+            :"right_arm" => ["\\", 3, 3],
+            :"left_leg" => ["/", 4, 1],
+            :"right_leg" => ["\\", 4, 3]
+        }
     end
 
     def display
@@ -24,5 +33,37 @@ class GameView
             print " " + letter 
         end
         puts "\n" 
+    end
+
+    def update_display(guess, mistakes)
+        update_word_display(guess)
+        update_hangman(mistakes)
+    end
+
+    def update_word_display(guess)
+        letter = nil
+        if guess
+            guess.each_with_index do |value, index|
+                if index == 0
+                    letter = value
+                else
+                    self.word_display[value] = letter  
+                end
+            end
+        end 
+    end
+
+    def update_hangman(mistakes)
+        body_parts = [:head, :torso, :left_arm, :right_arm, :left_leg, :right_leg]
+        if mistakes != 0
+            until mistakes == 0
+                mistakes = mistakes - 1
+                limb = body_parts[mistakes]
+                body_part = @hangman_parts[limb][0]
+                row = @hangman_parts[limb][1]
+                column = @hangman_parts[limb][2]
+                self.hangman_array[row][column] = body_part 
+            end
+        end
     end
 end
