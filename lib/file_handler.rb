d7c55cfac8b3298e2447828d703186bb5753188c
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-06-19 16:48:25 -0700

    Add the ability for users to name save files
    Also changed the file path for the saves sub
    directory to be placed in the data directory for
    organizational purposes.

--- lib/file_handler.rb
+++ lib/file_handler.rb
@@ -1,7 +1,7 @@
 require "json"
 
 module FileHandler
-    SAVE_DIRECTORY = "../Saves/"
+    SAVE_DIRECTORY = "../data/saves/"
 
     def load_save(game_file)
         string = File.read(game_file)
@@ -13,7 +13,18 @@ module FileHandler
         unless Dir.exist?(SAVE_DIRECTORY)
             Dir.mkdir SAVE_DIRECTORY
         end
-        File.open(SAVE_DIRECTORY + "test.json", "w") do |file|
+        file_name = nil
+        puts "\n"
+        until file_name
+            puts "Please type in your file name and include the \".json\" extension:"
+            input = gets.chomp
+            if input.include?(".json")
+                file_name = input
+            else
+                puts "\nInvalid file name, try again."
+            end
+        end
+        File.open(SAVE_DIRECTORY + file_name, "w") do |file|
             file.write(game.to_json)
         end
     end
